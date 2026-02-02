a0fe7cde5c814057a5f114457cf65a506b58f62d
bin-nmd-x <256811159+binary-nomad-x@users.noreply.github.com>
2026-02-02 19:28:17 +0500

    pivot table renamed, migration name corrected to nicer one

---
+++ prisma/migrations/20260202142519_update_post_categories_pivot_table_to_meaningful/migration.sql
@@ -0,0 +1,18 @@
+-- This is an empty migration.
+
+-- 1. Table ka naam badalna (_CategoryToPost -> post_categories)
+ALTER TABLE "_CategoryToPost" RENAME TO "post_categories";
+
+-- 2. Column 'A' ko 'category_id' mein badalna
+ALTER TABLE "post_categories" RENAME COLUMN "A" TO "category_id";
+
+-- 3. Column 'B' ko 'post_id' mein badalna
+ALTER TABLE "post_categories" RENAME COLUMN "B" TO "post_id";
+
+-- 4. Purane indexes aur constraints ko "Ache" naam dena (Optional but clean)
+ALTER TABLE "post_categories" RENAME CONSTRAINT "_CategoryToPost_AB_pkey" TO "post_categories_pkey";
+ALTER INDEX "_CategoryToPost_B_index" RENAME TO "post_categories_post_id_idx";
+
+-- 5. Foreign Keys ko rename karna taake professional lagein
+ALTER TABLE "post_categories" RENAME CONSTRAINT "_CategoryToPost_A_fkey" TO "post_categories_category_id_fkey";
+ALTER TABLE "post_categories" RENAME CONSTRAINT "_CategoryToPost_B_fkey" TO "post_categories_post_id_fkey";
