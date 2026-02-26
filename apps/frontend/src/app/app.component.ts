4c17f0561c8510f632dd07c2beafc453c6533c75
bin-nmd-x <256811159+binary-nomad-x@users.noreply.github.com>
2026-02-27 01:11:03 +0500

    trying to make the json single

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -3,10 +3,10 @@ import { RouterOutlet } from '@angular/router';
 import { DoctorsComponent } from './components/doctors/doctors.component';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    imports: [RouterOutlet, DoctorsComponent],
-    template: `
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, DoctorsComponent],
+  template: `
     <div style="max-width: 800px; margin: 0 auto;">
       <h1>Hospital Management System (Monorepo)</h1>
       <app-doctors></app-doctors>
@@ -15,5 +15,5 @@ import { DoctorsComponent } from './components/doctors/doctors.component';
   `,
 })
 export class AppComponent {
-    title = 'frontend';
+  title = 'frontend';
 }
