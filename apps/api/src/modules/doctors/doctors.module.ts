8304d20d64c94ced41cbde74f544d5fd47ae62f6
bin-nmd-x <256811159+binary-nomad-x@users.noreply.github.com>
2026-03-03 23:39:26 +0500

    unnecesary componnents, modules removed

--- apps/api/src/modules/doctors/doctors.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DoctorsService } from './doctors.service';
-import { DoctorsResolver } from './doctors.resolver';
-
-@Module({
-  providers: [DoctorsService, DoctorsResolver],
-})
-export class DoctorsModule {}
