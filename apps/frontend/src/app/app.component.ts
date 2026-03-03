63746a4e9d1eb616000a2c9809d118392d0ef81c
bin-nmd-x <256811159+binary-nomad-x@users.noreply.github.com>
2026-03-03 23:55:38 +0500

    changes

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DoctorsComponent } from './components/doctors/doctors.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DoctorsComponent],
+  imports: [RouterOutlet],
   template: `
     <div style="max-width: 800px; margin: 0 auto;">
       <h1>Hospital Management System (Monorepo)</h1>
-      <app-doctors></app-doctors>
       <router-outlet></router-outlet>
     </div>
   `,
