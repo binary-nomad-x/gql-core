32b5dd0d1e74fc391e5836fd00b620aaa25112a8
bin-nmd-x <256811159+binary-nomad-x@users.noreply.github.com>
2026-03-03 23:00:24 +0500

    unnecesary componnents, modules removed

--- apps/api/src/modules/appointments/appointments.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AppointmentsService } from './appointments.service';
-import { AppointmentsResolver } from './appointments.resolver';
-
-@Module({
-  providers: [AppointmentsService, AppointmentsResolver],
-})
-export class AppointmentsModule {}
