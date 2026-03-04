ed8baaa94232874b5eb1ec85bb90246a82d4de84
bin-nmd-x <256811159+binary-nomad-x@users.noreply.github.com>
2026-03-04 21:54:40 +0500

    change

--- apps/frontend/src/app/components/home/home.component.ts
+++ apps/frontend/src/app/components/home/home.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit {
+  ngOnInit(): void {}
 }
