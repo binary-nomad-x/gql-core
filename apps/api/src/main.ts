a6d52cf437db82c0b3924caf1080d7fbc4f01bf9
bin-nmd-x <256811159+binary-nomad-x@users.noreply.github.com>
2026-03-04 20:15:00 +0500

    formatted code

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,7 +1,6 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// import { join } from 'path';
 
 async function bootstrap() {
   console.log('DATABASE_URL:', process.env.DATABASE_URL);
@@ -12,5 +11,4 @@ async function bootstrap() {
   console.log(`GraphQL Playground: ${await app.getUrl()}/graphql`);
 }
 
-// Promises must be awaited, end with a call to .catch, end with a call to .then with a rejection handler or be explicitly marked as ignored with the `void` operator.
 bootstrap();
--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -5,12 +5,7 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  template: `
-    <div style="max-width: 800px; margin: 0 auto;">
-      <h1>Hospital Management System (Monorepo)</h1>
-      <router-outlet></router-outlet>
-    </div>
-  `,
+  templateUrl: 'app.component.html',
 })
 export class AppComponent {
   title = 'frontend';
