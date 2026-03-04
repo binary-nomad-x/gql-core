3dbb1de4d5f39e446ea710c08d78535a079140f3
bin-nmd-x <256811159+binary-nomad-x@users.noreply.github.com>
2026-03-05 01:36:26 +0500

    trying to show somehting #2

--- apps/frontend/src/app/components/profile/profile.component.ts
+++ apps/frontend/src/app/components/profile/profile.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
+  standalone: true,
   imports: [],
   template: ` <p>profile works!</p> `,
   styleUrl: './profile.component.scss',
 })
-export class ProfileComponent {}
+export class ProfileComponent { }
--- apps/frontend/src/main.ts
+++ apps/frontend/src/main.ts
@@ -1,3 +1,4 @@
+console.log('Main.ts starting...');
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
