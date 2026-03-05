c23c272ffbb8acd1fd2ebd6a3e9174df1d34c4d5
bin-nmd-x <256811159+binary-nomad-x@users.noreply.github.com>
2026-03-05 22:38:15 +0500

    minor change with update-user-input

--- apps/api/src/modules/user/dto/update-user.input.ts
+++ apps/api/src/modules/user/dto/update-user.input.ts
@@ -1,8 +1,9 @@
+import { randomUUID } from 'node:crypto';
 import { CreateUserInput } from './create-user.input';
 import { InputType, Field, Int, PartialType, ID } from '@nestjs/graphql';
 
 @InputType()
 export class UpdateUserInput extends PartialType(CreateUserInput) {
-  @Field(() => ID) // GraphQL ko batayein ke ye ID type hai
+  @Field(() => randomUUID) // GraphQL ko batayein ke ye ID type hai
   id!: string; // number ko string karein aur ! lagayein
 }
